fd07f319a35c43d91f8b03b9ccb05d81800d8379
Conner Swenberg <conner.swenberg@coinbase.com>
2026-01-13 11:09:52 -0800

    Add sample ERC-20 starter implmentation

---
+++ src/Donations.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {ERC20} from "./ERC20.sol";
+
+/// @notice Sample app for receiving ERC-20 donations
+contract Donations {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function donate(address token, uint256 amount) public {
+        // requires user to first set approval to this Donations contract
+        ERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(address token, address recipient) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        uint256 balance = ERC20(token).balanceOf(address(this));
+        ERC20(token).transfer(recipient, balance);
+    }
+}
